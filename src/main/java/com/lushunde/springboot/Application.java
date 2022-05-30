7124143590c9921515888edfb455d086faf08743
bellus <lushunde321@163.com>
2022-05-30 17:10:24 +0800

    log+swagger

--- src/main/java/com/lushunde/springboot/Application.java
+++ src/main/java/com/lushunde/springboot/Application.java
@@ -3,16 +3,12 @@ package com.lushunde.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-
-        System.out.println("args = " + Arrays.deepToString(args));
-        SpringApplication.run(Application.class, args);
-        System.out.println("Application.main");
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
 }
---
+++ src/main/java/com/lushunde/springboot/swagger_use_demo/User.java
@@ -0,0 +1,42 @@
+package com.lushunde.springboot.swagger_use_demo;
+
+import java.io.Serializable;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * 
+ * @模块名：springboot
+ * @包名：     model
+ * @类名称：User
+ * @类描述：用户信息
+ * @版本：      1.0
+ * @创建人：bellus
+ * @创建时间：2019年6月22日上午2:41:35
+ */
+@ApiModel(value="User",description="用户")
+public class User implements Serializable{
+	private static final long serialVersionUID = -1446919202047721900L;
+	@ApiModelProperty(name="userId",value="用户编号",dataType="Long")
+	private Long userId;
+	@ApiModelProperty(name="username",value="用户名称",dataType="String")
+	private String username;
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", username=" + username + "]";
+	}
+	
+}
